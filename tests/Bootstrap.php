9f411092fccdc3fbd444f633ac988ded3c3b9403
Dustin Thomson <dthomson@51systems.com>
2013-07-20 21:34:01 -0600

    Build changes

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 namespace ZF2Deploy\Tests;//Change this namespace for your test
 
+use Composer\Autoload\ClassLoader;
 use Zend\Loader\AutoloaderFactory;
 use Zend\Mvc\Service\ServiceManagerConfig;
 use Zend\ServiceManager\ServiceManager;
@@ -18,82 +19,26 @@ class Bootstrap
 
     public static function init()
     {
-        // Load the user-defined test configuration file, if it exists; otherwise, load
-        if (is_readable(__DIR__ . '/TestConfig.php')) {
-            $testConfig = include __DIR__ . '/TestConfig.php';
-        } else {
-            $testConfig = include __DIR__ . '/TestConfig.php.dist';
-        }
-
-        $zf2ModulePaths = array();
-
-        if (isset($testConfig['module_listener_options']['module_paths'])) {
-            $modulePaths = $testConfig['module_listener_options']['module_paths'];
-            foreach ($modulePaths as $modulePath) {
-                if (($path = static::findParentPath($modulePath)) ) {
-                    $zf2ModulePaths[] = $path;
-                }
-            }
-        }
-
-        $zf2ModulePaths  = implode(PATH_SEPARATOR, $zf2ModulePaths) . PATH_SEPARATOR;
-        $zf2ModulePaths .= getenv('ZF2_MODULES_TEST_PATHS') ?: (defined('ZF2_MODULES_TEST_PATHS') ? ZF2_MODULES_TEST_PATHS : '');
-
         static::initAutoloader();
 
-        // use ModuleManager to load this module and it's dependencies
-        $baseConfig = array(
-            'module_listener_options' => array(
-                'module_paths' => explode(PATH_SEPARATOR, $zf2ModulePaths),
-            ),
-        );
-
-        $config = ArrayUtils::merge($baseConfig, $testConfig);
-
-        $serviceManager = new ServiceManager(new ServiceManagerConfig());
-        $serviceManager->setService('ApplicationConfig', $config);
-        $serviceManager->get('ModuleManager')->loadModules();
-
-        static::$serviceManager = $serviceManager;
-        static::$config = $config;
     }
 
-    public static function getServiceManager()
-    {
-        return static::$serviceManager;
-    }
-
-    public static function getConfig()
-    {
-        return static::$config;
-    }
 
     protected static function initAutoloader()
     {
         $vendorPath = static::findParentPath('vendor');
 
         if (is_readable($vendorPath . '/autoload.php')) {
+            /** @var ClassLoader $loader */
             $loader = include $vendorPath . '/autoload.php';
+
+            $loader->addClassMap(array(
+                'ZF2Deploy' => 'src/'
+            ));
+
         } else {
-            $zf2Path = getenv('ZF2_PATH') ?: (defined('ZF2_PATH') ? ZF2_PATH : (is_dir($vendorPath . '/ZF2/library') ? $vendorPath . '/ZF2/library' : false));
-
-            if (!$zf2Path) {
-                throw new RuntimeException('Unable to load ZF2. Run `php composer.phar install` or define a ZF2_PATH environment variable.');
-            }
-
-            include $zf2Path . '/Zend/Loader/AutoloaderFactory.php';
-
+            throw \Exception('Cannot locate vendor autoload file');
         }
-
-        AutoloaderFactory::factory(array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'autoregister_zf' => true,
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/' . __NAMESPACE__,
-                    'ZF2Deploy' => __DIR__ . '/../src/ZF2Deploy'
-                ),
-            ),
-        ));
     }
 
     protected static function findParentPath($path)

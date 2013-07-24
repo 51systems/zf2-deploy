29714a28c08c72d6848d325bbaaafcb0adf76cf2
Dustin Thomson <dthomson@51systems.com>
2013-07-24 17:17:39 -0600

    Fixed fatal error in the case where no plugins were executed

--- src/ZF2Deploy/Executor/Html.php
+++ src/ZF2Deploy/Executor/Html.php
@@ -56,6 +56,9 @@ class Html extends AbstractExecutor
      */
     public function getLogOutput()
     {
+        if ($this->logWriter == null)
+            return "";
+
         return $this->logWriter->getLogString();
     }
 
